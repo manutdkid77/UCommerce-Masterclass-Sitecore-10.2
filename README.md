f1def6133b322406214e8489093c85c299d5a621
Nathaniel Nunes <manutdkid77@gmail.com>
2023-01-13 19:05:54 +0530

    implement different search methods and add sc content package

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Ucommerce.Masterclass.Sitecore
-Ucommerce Masterclass repo for Sitecore 
+# Learning Ucommerce for Sitecore 10,2
+
+This is a simple Sitecore 10.2 project that follows along with the [Ucommerce for Sitecore MasterClass training course](https://academy.ucommerce.net/training/sitecore/).
+
+## Setup
+
+* This project runs on Sitecore 10.2 and .NET Framework 4.8
+* Sitecore Content packages available in the **content packages** folder in the project root. Import them into Sitecore.
+* Create a database in sql server called `ucommerce`
+* Specify the connection strings in `ConnectionStrings.config` file of your cm server.
+
+    `<add name="ucommerce" connectionString="Data Source=[SQLSERVER_NAME];Initial Catalog=ucommerce;User ID=[username];Password=[password]" />`
+
+## References
+
+### GitHub Repos
+[Ucommerce.Masterclass.Sitecore](https://github.com/Ucommercenet/Ucommerce.Masterclass.Sitecore)
+[Ucommerce Umbraco](https://github.com/Ucommercenet/Ucommerce.Masterclass)
+[Ucommerce.Sitecore](https://github.com/Ucommercenet/Ucommerce.Sitecore)
+[Avenue-Clothing-For-Sitecore](https://github.com/Ucommercenet/Avenue-Clothing-For-Sitecore/)
+
+
+### Doc Links
+[Ucommerce Product Definition](https://docs.ucommerce.net/ucommerce/v9.6.1/getting-started/catalog-foundation/product-definitions.html)
+[Access Catalog Library](https://docs.ucommerce.net/ucommerce/v9.6.1/getting-started/catalog-foundation/catalog-library.html)
+[Indexing](https://docs.ucommerce.net/ucommerce/v9.6.1/search-and-indexing/indexing/indexing-basics.html)
+[ProductDefintion Index Location for Sitecore Porjects](https://github.com/Ucommercenet/Avenue-Clothing-For-Sitecore/blob/4450900ebcc44f2b0ac6aa67561d393eeae4f103/src/AvenueClothing.Project.Website/sitecore%20modules/Shell/Ucommerce/Apps/Ucommerce.Search.Lucene.Avenue/Configuration/AvenueProductsIndexDefinition.config)
+[Ucommerce Search API](https://docs.ucommerce.net/ucommerce/v9.6.1/search-and-indexing/Search-api.html)
+[Faceted Search](https://www.addsearch.com/blog/faceted-search/)
+[Search Facet](https://www.loop54.com/blog/what-is-a-search-facet-what-is-a-search-filter)
+[Pricing Structure](https://docs.ucommerce.net/ucommerce/v9.6.1/getting-started/catalog-foundation/Pricing-structure.html)
--- src/Ucommerce.Masterclass.Sitecore/Ucommerce.Masterclass.Sitecore/Controllers/MasterClassProductSearchResultController.cs
+++ src/Ucommerce.Masterclass.Sitecore/Ucommerce.Masterclass.Sitecore/Controllers/MasterClassProductSearchResultController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web.Mvc;
 using Ucommerce.Api;
 using Ucommerce.Infrastructure;
-using Ucommerce.Masterclass.Models;
 using Ucommerce.Search;
 using Ucommerce.Search.Models;
 using Ucommerce.Search.Slugs;
@@ -23,10 +22,10 @@ namespace Ucommerce.Masterclass.Models
         {
             return Request.QueryString["Query"];
         }
-        
+
         public MasterClassProductSearchResultController()
         {
-            
+
         }
 
         public static class Constants
@@ -34,37 +33,51 @@ namespace Ucommerce.Masterclass.Models
             public static System.Guid Guid = System.Guid.Parse("92CDC7C2-B511-41C6-ADF5-37E66CD52366");
         }
 
-        public ActionResult Index()
+        public ActionResult MasterClassProductSearchResult()
         {
             var model = new ProductListViewModel();
 
             var searchTerm = GetSearchTerm();
 
+            //resolve and access the search index for Products
             var index = ObjectFactory.Instance.Resolve<IIndex<Ucommerce.Search.Models.Product>>();
 
             var guid = System.Guid.Parse("92CDC7C2-B511-41C6-ADF5-37E66CD52366");
 
-            var result = index.Find()
-                .Where(
-                    product => product.VariantSku == null
-                    && product.ParentProduct == null
-                    && product.ProductDefinition == Constants.Guid 
-                    &&
-                    (product.Sku.Contains(searchTerm)
-                        || product.DisplayName == Match.FullText(searchTerm)
-                        || product.ShortDescription == Match.FullText(searchTerm)
-                        || product.LongDescription == Match.FullText(searchTerm)
-                    )).ToList();
+            //this is lower level code implementation of CatalogLibrary.GetProducts()
+            //https://docs.ucommerce.net/ucommerce/v9.6.1/search-and-indexing/Search-api.html
+            var results = index.Find<Ucommerce.Search.Models.Product>()
+                .Where(p =>
+                           p.DisplayName == Match.Wildcard($"*{searchTerm}*") //similar to SQL LIKE operator: %searchterm%
+                        || p.DisplayName == Match.FullText(searchTerm)
+                        || p.ShortDescription == Match.FullText(searchTerm)
+                        || p.Name == Match.Fuzzy(searchTerm, 2)
+                        || p.Sku == Match.Literal(searchTerm)
+                    ).ToList().Results;
 
-            model.ProductViewModels = MapProducts(result.Results);
-            return View(model);
-            
+            //we can also combine the above query with the facets selected by user (overheard involved)
+
+            //var result = index.Find<Ucommerce.Search.Models.Product>()
+            //    .Where(
+            //        product => product.VariantSku == null
+            //        && product.ParentProduct == null
+            //        && product.ProductDefinition == Constants.Guid 
+            //        &&
+            //        (product.Sku.Contains(searchTerm) //Sku doesnt support FullText so use .Contains()
+            //            || product.DisplayName == Match.FullText(searchTerm)
+            //            || product.ShortDescription == Match.FullText(searchTerm)
+            //            || product.LongDescription == Match.FullText(searchTerm)
+            //        )).ToList();
+
+            model.ProductViewModels = MapProducts(results);
+            return View("/Views/MasterClassProductSearchResult/Index.cshtml", model);
+
         }
-        
+
         private IList<ProductViewModel> MapProducts(IList<Product> products)
         {
             var prices = CatalogLibrary.CalculatePrices(products.Select(x => x.Guid).ToList());
-            
+
             return products.Select(product => new ProductViewModel()
             {
                 LongDescription = product.LongDescription,
